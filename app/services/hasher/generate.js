cf410887199e2ccd36162cf0b228135af1e88494
milan-mooibroek <Broertjeoktober6>
2020-08-31 08:09:31 +0200

    Optimized hash generator. Can now count more than 3 characters.

--- app/services/hasher/generate.js
+++ app/services/hasher/generate.js
@@ -1,42 +1,24 @@
 const characters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 module.exports.generateHash = function (id) {
-
-    const calc = characters.length;
-    let counter = 1;
-    let characterCounter = 0;
-    let innerCounter = 0;
-    let sum = characters.length;
     let hash = '';
-
-    //Todo with this length u can calculate the string length and position
-    while (sum <= id) {
-        counter++;
-        sum *= calc;
-    }
-
-    //Loop trough characters to set new hashes.
-    for (let i = 0; i < id; i++) {
-        if (characterCounter >= characters.length)  characterCounter = 0;
-        if (characterCounter === 0
-            && id > characters.length) {
-            if (hash.length === 0) {
-                hash += characters[characterCounter];
-            } else  {
-                innerCounter++;
-                //Todo fix this if statement to support 3 string > combinations.
-                if (innerCounter < (characters.length) ) {
-                    hash = hash.slice(0, -1) + characters[innerCounter];
+    let hashArray = [id];
+    while (hashArray.some(function(id) {
+        return id > characters.length;
+    })) {
+        hashArray.forEach(function (id, i, ids) {
+            if (id > characters.length) {
+                ids[i] -= characters.length;
+                if (ids[(i + 1)]) {
+                    ids[(i + 1)] += 1;
                 } else {
-                    hash += characters[characterCounter];
-                    innerCounter = 0;
+                    ids.push(1);
                 }
             }
-        }
-        if (i === (id - 1)) {
-            hash += characters[characterCounter];
-        }
-        characterCounter++
+        });
     }
+    hashArray.forEach(function (id) {
+        hash += characters[(id - 1)];
+    })
     return hash
 };
